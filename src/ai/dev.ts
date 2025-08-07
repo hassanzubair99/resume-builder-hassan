03944ae2d9033dc76f6af1b4ae19941befdbb184
hassan zubair <hassanzubair4243@gmail.com>
2025-08-07 17:16:27 +0000

    ????\

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,5 +1,7 @@
-import { config } from 'dotenv';
-config();
+'use server';
+
+import {config} from 'dotenv';
+config({path: '.env.local'});
 
 import '@/ai/flows/optimize-resume-content.ts';
 import '@/ai/flows/enhance-resume.ts';
